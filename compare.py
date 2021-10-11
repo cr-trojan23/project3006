61465fdd7a766ddf3a9b5cb894106166b49bc793
cr-trojan23 <pandusrinu1604@gmail.com>
2021-10-11 23:13:12 +0530

    (feat): Added S3 interaction, improved formatting

--- compare.py
+++ compare.py
@@ -3,53 +3,64 @@ import cv2
 import time
 from botocore.exceptions import ClientError
 
+# For new user registration
+
+
 def registerFaces(targetCapture):
-    s3 = boto3.client('s3')
-    bucket = 'project3006'
+    s3 = boto3.client('s3')  # S3 client (S3 is an object storage service)
+    bucket = 'project3006'  # Bucket name (Folder to store images in S3)
     try:
+        # Upload targetCapture image to project3006 bucket with name someuser1.jpg
         s3.upload_file(targetCapture, bucket, 'someuser1.jpg')
-    except ClientError as e:
+    except ClientError as e:  # If error, print error
         print(e)
 
+# For comparing faces
+
+
 def compareFaces(target):
+    # Rekognition client (Rekognition is a service that recognizes faces in images)
     rekognition = boto3.client('rekognition')
     imageSource = {
         'S3Object': {
             'Bucket': 'project3006',
             'Name': 'someuser1.jpg'
         }
-    }
-    imageTarget = open(target, 'rb')
+    }  # Image of user taken when registering
+    imageTarget = open(target, 'rb')  # Image of user taken when comparing
     response = rekognition.compare_faces(SimilarityThreshold=80,
-                                        SourceImage=imageSource,
-                                        TargetImage={'Bytes': imageTarget.read()})
+                                         SourceImage=imageSource,
+                                         TargetImage={'Bytes': imageTarget.read()})  # Compare faces
 
-    for faceMatch in response['FaceMatches']:
+    for faceMatch in response['FaceMatches']:  # For each face match
+        # Get confidence of match
         confidence = str(faceMatch['Face']['Confidence'])
-        print('The face matches with ' + confidence + '% confidence')
-        
-    imageTarget.close()
+        print('The face matches with ' + confidence +
+             '% confidence')  # Print confidence
+
+    imageTarget.close()  # Close imageTarget
 
 
+# Capture image from webcam
 def capture():
-    camera = cv2.VideoCapture(0)
-    ret, frame = camera.read()
-    time.sleep(2)
-    cv2.imwrite('/tmp/img.jpg', frame)
-    camera.release()
+    camera = cv2.VideoCapture(0)  # Open webcam
+    ret, frame = camera.read()  # Read image from webcam
+    time.sleep(2)  # Wait 2 seconds
+    cv2.imwrite('/tmp/img.jpg', frame)  # Save image to /tmp/img.jpg
+    camera.release()  # Release webcam
 
 
 def main():
     print("What do you want to do?\n 1. Register\n 2. Compare\n 3. Exit")
     choice = input()
     if choice == '1':
-        capture()
-        targetCapture = '/tmp/img.jpg'
-        registerFaces(targetCapture)
+        capture()  # Capture image from webcam
+        targetCapture = '/tmp/img.jpg'  # Target image
+        registerFaces(targetCapture)  # Register image
     elif choice == '2':
-        capture()
-        targetCapture = '/tmp/img.jpg'
-        compareFaces(targetCapture)
+        capture()  # Capture image from webcam
+        targetCapture = '/tmp/img.jpg'  # Target image
+        compareFaces(targetCapture)  # Compare image
     elif choice == '3':
         exit()
 
