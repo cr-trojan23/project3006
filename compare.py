d25f83a5b8adc1b414f633353bc999743386702a
cr-trojan23 <pandusrinu1604@gmail.com>
2021-10-11 23:04:21 +0530

    (feat): Added S3 interaction

--- compare.py
+++ compare.py
@@ -1,30 +1,58 @@
 import boto3
+import cv2
+import time
+from botocore.exceptions import ClientError
 
-KEY_SOURCE = "/home/kiaria/Downloads/source.jpeg"
-KEY_TARGET = "/home/kiaria/Downloads/target.jpeg"
+def registerFaces(targetCapture):
+    s3 = boto3.client('s3')
+    bucket = 'project3006'
+    try:
+        s3.upload_file(targetCapture, bucket, 'someuser1.jpg')
+    except ClientError as e:
+        print(e)
 
-
-def compare_faces(source, target):
-    client = boto3.client('rekognition')
-    imageSource = open(source, 'rb')
+def compareFaces(target):
+    rekognition = boto3.client('rekognition')
+    imageSource = {
+        'S3Object': {
+            'Bucket': 'project3006',
+            'Name': 'someuser1.jpg'
+        }
+    }
     imageTarget = open(target, 'rb')
-
-    response = client.compare_faces(SimilarityThreshold=80,
-                                    SourceImage={'Bytes': imageSource.read()},
-                                    TargetImage={'Bytes': imageTarget.read()})
+    response = rekognition.compare_faces(SimilarityThreshold=80,
+                                        SourceImage=imageSource,
+                                        TargetImage={'Bytes': imageTarget.read()})
 
     for faceMatch in response['FaceMatches']:
         confidence = str(faceMatch['Face']['Confidence'])
         print('The face matches with ' + confidence + '% confidence')
-
-    imageSource.close()
+        
     imageTarget.close()
-    return response['FaceMatches']
+
+
+def capture():
+    camera = cv2.VideoCapture(0)
+    ret, frame = camera.read()
+    time.sleep(2)
+    cv2.imwrite('/tmp/img.jpg', frame)
+    camera.release()
 
 
 def main():
-    face_matches = compare_faces(KEY_SOURCE, KEY_TARGET)
+    print("What do you want to do?\n 1. Register\n 2. Compare\n 3. Exit")
+    choice = input()
+    if choice == '1':
+        capture()
+        targetCapture = '/tmp/img.jpg'
+        registerFaces(targetCapture)
+    elif choice == '2':
+        capture()
+        targetCapture = '/tmp/img.jpg'
+        compareFaces(targetCapture)
+    elif choice == '3':
+        exit()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
